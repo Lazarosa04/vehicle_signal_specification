b02ce4b10b5bdde8039df47d5f763c41fcc67604
erikbosch <30996601+erikbosch@users.noreply.github.com>
2025-01-22 07:53:43 +0000

    Auto-deploy docs built from commit 147467ed86db52f6c0f2d69e4521ab9d0918f7ec

--- index.search.js
+++ index.search.js
@@ -105,8 +105,8 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "Vehicle Signal Specification",
-    "content": "Discover what the Vehicle Signal Specification is all about.",
-    "description": "Discover what the Vehicle Signal Specification is all about.",
+    "content": "VSS Tree Model The diagram below shows a generated tree of the VSS. Here you can see a mapping for the current catalog of signals related to vehicles. Click a Branch Node to expand or collapse the tree. (Slower animation while pressing the altKey (“option” key for Mac OS-X).)\nCurrent Population of the model - in respect to VSS Types and Datatypes Click on the nodes to highlight the links, or hover over the links to see their exact population.\nReleases and types How the model changed in major releases.",
+    "description": "VSS Tree Model The diagram below shows a generated tree of the VSS. Here you can see a mapping for the current catalog of signals related to vehicles. Click a Branch Node to expand or collapse the tree. (Slower animation while pressing the altKey (“option” key for Mac OS-X).)\nCurrent Population of the model - in respect to VSS Types and Datatypes Click on the nodes to highlight the links, or hover over the links to see their exact population.",
     "tags": [],
     "title": "Introduction",
     "uri": "/vehicle_signal_specification/introduction/index.html"
